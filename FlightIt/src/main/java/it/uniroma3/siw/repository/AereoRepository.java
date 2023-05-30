2e2cb2155588d180347f8fa83d566763df9767ab
GIACOMO CUI <gia.cui@stud.uniroma3.it>
2023-05-30 09:46:31 +0200

    Modifiche fatte in aula, aggiunta di commenti ai vari template con indicazioni su cosa contengono e eliminazione di classi non nesessarie

--- FlightIt/src/main/java/it/uniroma3/siw/repository/AereoRepository.java
+++
@@ -1,7 +0,0 @@
-package it.uniroma3.siw.repository;
-
-import it.uniroma3.siw.model.Aereo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AereoRepository extends CrudRepository<Aereo, Long> {
-}
--- FlightIt/src/main/java/it/uniroma3/siw/repository/AereoportoRepository.java
+++
@@ -1,6 +0,0 @@
-package it.uniroma3.siw.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import it.uniroma3.siw.model.Aereoporto;
-public interface AereoportoRepository extends CrudRepository<Aereoporto, Long>{
-}
