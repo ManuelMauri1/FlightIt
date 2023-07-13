6e9465685b4e5168f03ffc2bb02d5104f617fae3
Manuel <man.mauri@stud.uniroma3.it>
2023-07-13 19:35:29 +0200

    IGNORA

---
+++ FlightIt/src/main/java/it/uniroma3/siw/controller/validator/VoloValidator.java
@@ -0,0 +1,28 @@
+package it.uniroma3.siw.controller.validator;
+
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.model.Volo;
+import it.uniroma3.siw.repository.VoloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class VoloValidator implements Validator {
+    @Autowired
+    private VoloRepository voloRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Volo.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "aereoportoP", "NotBlank");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "aereoportoA", "NotBlank");
+        Volo volo = (Volo) target;
+    }
+}
