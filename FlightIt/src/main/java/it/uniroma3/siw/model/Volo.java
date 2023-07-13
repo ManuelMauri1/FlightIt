346d59123ff208388f37c82cc887a0dac411ba85
Manuel <man.mauri@stud.uniroma3.it>
2023-07-13 19:03:37 +0200

    GENERAZIONE CODICE VOLO

--- FlightIt/src/main/java/it/uniroma3/siw/model/Volo.java
+++ FlightIt/src/main/java/it/uniroma3/siw/model/Volo.java
@@ -4,23 +4,55 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.security.SecureRandom;
 import java.sql.Time;
 import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Random;
 
 @Data
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
 public class Volo {
+    private static final int CODICE = 10;
+    private static final String CARATTERI = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private Integer codiceVolo;
+    private String codiceVolo;
     private LocalDate dataPartenza;
     private Time oraPartenza;
     private Time oraArrivo;
-
     private String aereoportoPartenza;
-
     private String aereoportoArrivo;
+
+    public Volo(){
+        this.codiceVolo = generaCodiceVolo();
+    }
+
+    public static String generaCodiceVolo() {
+        StringBuilder sb = new StringBuilder(CODICE);
+        Random random = new SecureRandom();
+
+        for (int i = 0; i < CODICE; i++) {
+            int randomIndex = random.nextInt(CARATTERI.length());
+            char randomChar = CARATTERI.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Volo volo)) return false;
+        return Objects.equals(getCodiceVolo(), volo.getCodiceVolo()) && Objects.equals(getDataPartenza(), volo.getDataPartenza());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodiceVolo(), getDataPartenza());
+    }
 }
