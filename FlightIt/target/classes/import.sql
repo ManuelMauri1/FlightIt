f4af06fc5b8f5b3a95e36c46f76e898e1c690758
Giacomo Cui <giacomocui2012@gmail.com>
2023-07-16 00:11:26 +0200

    Ha ripreso a funzionare la pagina dei voli. Design da ultimare

--- FlightIt/target/classes/import.sql
+++ FlightIt/target/classes/import.sql
@@ -4,6 +4,6 @@ insert into aereoporto (id, nome) values(2, 'Fiumicino');
 insert into aereoporto (id, nome) values(3, 'Malpensa');
 
 /* INSER VOLO */
-insert into volo (id, codice_volo, data_partenza, ora_arrivo, ora_partenza, aereoporto_arrivo, aereoporto_partenza) values(1, 'RziMLdyfKt', '2023-07-04', '06:20:00', '04:15:00', 2, 3);
+insert into volo (id, codice_volo, data_partenza, ora_arrivo, ora_partenza, aereoporto_arrivo_id, aereoporto_partenza_id) values(1, 'RziMLdyfKt', '2023-07-04', '06:20:00', '04:15:00', 2, 3);
 
-insert into volo (id, codice_volo, data_partenza, ora_arrivo, ora_partenza, aereoporto_arrivo, aereoporto_partenza) values(2, '2HstcHvHPQ', '2023-07-18', '09:15:00', '08:15:00', 3, 2);
+insert into volo (id, codice_volo, data_partenza, ora_arrivo, ora_partenza, aereoporto_arrivo_id, aereoporto_partenza_id) values(2, '2HstcHvHPQ', '2023-07-18', '09:15:00', '08:15:00', 3, 2);
