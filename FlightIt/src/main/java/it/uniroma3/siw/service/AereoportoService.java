d511843dbb7f8c4bf04ce09611506670466c8d53
Giacomo Cui <giacomocui2012@gmail.com>
2023-07-15 23:06:57 +0200

    Ignora

--- FlightIt/src/main/java/it/uniroma3/siw/service/AereoportoService.java
+++ FlightIt/src/main/java/it/uniroma3/siw/service/AereoportoService.java
@@ -1,6 +1,7 @@
 package it.uniroma3.siw.service;
 
 import it.uniroma3.siw.model.Aereoporto;
+import it.uniroma3.siw.model.Volo;
 import it.uniroma3.siw.repository.AereoportoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,44 @@ public class AereoportoService {
     @Autowired
     private AereoportoRepository aereoportoRepository;
 
-    @Transactional
-    public List<Aereoporto> getAereoporti(){
+    public Aereoporto getAereoportoByNome(String nomeAereoporto) {
+        return aereoportoRepository.findByNome(nomeAereoporto);
+    }
+
+    public List<Aereoporto> getAereoporti() {
         return aereoportoRepository.findAll();
     }
+
+    @Transactional
+    public void aggiungiVolo(Volo volo, String aereoportoA, String aereoportoP) {
+        aggiungiVoloInEntrata(volo, aereoportoA);
+        aggiungiVoloInUscita(volo, aereoportoP);
+    }
+
+    public void aggiungiVoloInEntrata(Volo volo, String aereoportoA) {
+        getAereoportoByNome(aereoportoA).getVoliInEntrata().add(volo);
+    }
+
+
+    public void aggiungiVoloInUscita(Volo volo, String aereoportoP) {
+        getAereoportoByNome(aereoportoP).getVoliInUscita().add(volo);
+    }
+
+    @Transactional
+    public void cancellaVoli(List<Volo> voliDaId) {
+        for (Volo volo : voliDaId) {
+            eliminaVoloInUscita(volo, volo.getAereoportoPartenza());
+            eliminaVoloInEntrata(volo, volo.getAereoportoArrivo());
+        }
+    }
+
+
+    public void eliminaVoloInEntrata(Volo volo, Aereoporto aereoportoA) {
+        aereoportoA.getVoliInEntrata().remove(volo);
+    }
+
+    public void eliminaVoloInUscita(Volo volo, Aereoporto aereoportoP) {
+        aereoportoP.getVoliInUscita().remove(volo);
+    }
+
 }
