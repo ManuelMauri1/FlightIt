4bc50dc32b8ca9c5f9d991e32cd3c6955d3088df
Manuel <man.mauri@stud.uniroma3.it>
2023-07-15 19:23:29 +0200

    IMPORT

--- FlightIt/src/main/resources/import.sql
+++ FlightIt/src/main/resources/import.sql
@@ -3,6 +3,11 @@ insert into aereoporto (id, nome) values(1, 'Ciampino');
 insert into aereoporto (id, nome) values(2, 'Fiumicino');
 insert into aereoporto (id, nome) values(3, 'Malpensa');
 
+/* INSER VOLO */
+insert into volo (id, codice_volo, data_partenza, ora_arrivo, ora_partenza, aereoporto_arrivo_id, aereoporto_partenza_id) values(1, 'RziMLdyfKt', '2023-07-04', '06:20:00', '04:15:00', 2, 3);
+
+insert into volo (id, codice_volo, data_partenza, ora_arrivo, ora_partenza, aereoporto_arrivo_id, aereoporto_partenza_id) values(2, '2HstcHvHPQ', '2023-07-18', '09:15:00', '08:15:00', 3, 2);
+
 /* INSERT UTENTE - CREDENTIALS */
 insert into utente (id, nome) values(1, 'ManuelMauri');
 insert into credentials (id, auth_provider, ruolo, username, utente_id) values(1, 'GITHUB', 'ADMIN', 'ManuelMauri1', 1);
